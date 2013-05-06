85e32ed79906609dd2feb586c76a30963b533349
Max Fest <fest@dragonwar.fr>
2013-05-06 19:26:12 +0000

    Server: HandleInput, besoin d'un client pour test

--- Server/SessionSocket.cpp
+++ Server/SessionSocket.cpp
@@ -5,7 +5,7 @@
 ** Login  <ginter_m@epitech.eu>
 **
 ** Started on  Mon May 06 17:26:18 2013 maxime ginters
-** Last update Mon May 06 18:35:29 2013 maxime ginters
+** Last update Mon May 06 19:24:39 2013 maxime ginters
 */
 
 #include "SessionSocket.h"
@@ -24,5 +24,27 @@ tcp::socket& SessionSocket::socket()
 void SessionSocket::OnOpen()
 {
     std::cout << "SessionSocket start" << std::endl;
+}
+
+void SessionSocket::HandleInput(boost::system::error_code const& error, std::size_t bytes_transferred)
+{
+    if (error)
+    {
+        std::cout << "Error: " << error.message() << std::endl;
+        _socket.close();
+        return;
+    }
+    (void)bytes_transferred;
+    (void)_session;
+    std::cout << "HandleInput" << std::endl;
+    _RegisterRead();
+}
+
+void SessionSocket::_RegisterRead()
+{
+    _socket.async_read_some(boost::asio::buffer(buffer, SOCKET_BUFFER_SIZE),
+            boost::bind(&SessionSocket::HandleInput, this,
+                boost::asio::placeholders::error,
+                boost::asio::placeholders::bytes_transferred));
 }
 
--- Server/SessionSocket.h
+++ Server/SessionSocket.h
@@ -5,11 +5,13 @@
 ** Login  <ginter_m@epitech.eu>
 **
 ** Started on  Mon May 06 17:26:10 2013 maxime ginters
-** Last update Mon May 06 18:35:36 2013 maxime ginters
+** Last update Mon May 06 19:24:38 2013 maxime ginters
 */
 
 #ifndef SESSIONSOCKET_H_
 # define SESSIONSOCKET_H_
+
+#define SOCKET_BUFFER_SIZE 4096
 
 #include <boost/asio.hpp>
 #include "Shared.h"
@@ -26,9 +28,13 @@ public:
     tcp::socket& socket();
 
     void OnOpen();
+
+    void HandleInput(boost::system::error_code const& error, std::size_t reicvSize);
 private:
+    void _RegisterRead();
     tcp::socket _socket;
     Session* _session;
+    char buffer[SOCKET_BUFFER_SIZE];
 };
 
 #endif /* !SESSIONSOCKET_H_ */
