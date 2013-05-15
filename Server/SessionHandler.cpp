4276419f8280932a8c4ef83375d492ddc1060aa0
Max Fest <fest@dragonwar.fr>
2013-05-15 17:49:01 +0000

    Fix map size

--- Server/SessionHandler.cpp
+++ Server/SessionHandler.cpp
@@ -5,7 +5,7 @@
 ** Login  <ginter_m@epitech.eu>
 **
 ** Started on  Fri May 10 15:42:46 2013 maxime ginters
-** Last update Tue May 14 17:52:14 2013 maxime ginters
+** Last update Wed May 15 17:48:38 2013 maxime ginters
 */
 
 #include "Session.h"
@@ -40,8 +40,8 @@ void Session::HandleEnterGame(Packet& recvData)
     map->GetWidthAndHeight(width, height);
 
     Packet data(SMSG_ADD_TO_MAP, 24);
-    data << uint32(width);
-    data << uint32(height);
+    data << uint32(width * MAP_PRECISION);
+    data << uint32(height * MAP_PRECISION);
     data << uint32(_player->GetModelId());
     _player->UpdatePosition(5.0f, 5.0f, 0.0f, 0.0f);
     _player->WritePosition(data);
