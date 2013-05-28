a9575ae0f61db458268ec3e00f4c154a8c349e87
Max Fest <fest@dragonwar.fr>
2013-05-28 19:47:58 +0200

    Fix SHaredPTR

--- Shared/SharedPtr.hpp
+++ Shared/SharedPtr.hpp
@@ -5,99 +5,115 @@
 ** Login  <ginter_m@epitech.eu>
 **
 ** Started on  Fri May 17 16:33:15 2013 maxime ginters
-** Last update Fri May 17 17:20:20 2013 maxime ginters
+** Last update Tue May 28 19:46:01 2013 maxime ginters
 */
 
 #ifndef SHAREDPTR_H_
 # define SHAREDPTR_H_
 
+#include "ScopLock.h"
 #include "SharedDefines.h"
 
 template<class T>
 class SharedPtr
 {
 public:
-    SharedPtr() :
-        _ptr(NULL), _count(NULL)
-    {}
-
-    SharedPtr(T* ptr) :
-        _ptr(ptr), _count(NULL)
+    SharedPtr() : _data(NULL), _ref(NULL)
     {
-        _count = new uint32;
-        *_count = 0;
+        _ref = new RefCounter();
+        _ref->Add();
     }
 
-    SharedPtr(SharedPtr<T> const& other) :
-        _ptr(other._ptr), _count(other._count)
+    SharedPtr(T* val) : _data(val), _ref(NULL)
     {
-        if (_count)
-           *_count += 1;
+        _ref = new RefCounter();
+        _ref->Add();
     }
 
-    SharedPtr<T>& operator=(SharedPtr<T> const& other)
+    SharedPtr(SharedPtr<T> const& other) : _data(other._data), _ref(other._ref)
     {
-        _ptr = other._ptr;
-        _count = other._count;
-        if (_count)
-            *_count += 1;
-        return *this;
-    }
-
-    SharedPtr<T>& operator=(T* other)
-    {
-        _ptr = other;
-        _count = new uint32;
-        *_count = 0;
-        return *this;
+        _ref->Add();
     }
 
     ~SharedPtr()
     {
-        if (!_count)
-            return;
-        if(*_count == 0)
+        if (_ref->Release() == 0)
         {
-            delete _ptr;
-            delete _count;
+            delete _data;
+            delete _ref;
         }
-        else
-            *_count -= 1;
     }
 
-    bool operator==(SharedPtr<T> const& other)
+    T& operator*()
     {
-        return _ptr == other._ptr;
+        return *_data;
     }
 
     T* operator->()
     {
-        return _ptr;
+        return _data;
     }
 
-    T const* operator->() const
+    SharedPtr<T>& operator=(SharedPtr<T> const& other)
     {
-        return _ptr;
-    }
+        if (this != &other)
+        {
+            if (_ref->Release() == 0)
+            {
+                delete _data;
+                delete _ref;
+            }
 
-    T operator*()
-    {
-        return *_ptr;
-    }
-
-    bool operator!() const
-    {
-        return _ptr == NULL;
+            _data = other._data;
+            _ref = other._ref;
+            _ref->Add();
+        }
+        return *this;
     }
 
     T* get()
     {
-        return _ptr;
+        return _data;
     }
 
+
+    bool operator==(SharedPtr<T> const& other)
+    {
+        return _data == other._data;
+    }
+
+    T const* operator->() const
+    {
+        return _data;
+    }
+
+    bool operator!() const
+    {
+        return _data == NULL;
+    }
+
+
 private:
-    T* _ptr;
-    uint32* _count;
+    class RefCounter
+    {
+    public:
+        void Add()
+        {
+            ScopLock lock(_mutex);
+            ++_count;
+        }
+        uint32 Release()
+        {
+            ScopLock lock(_mutex);
+            return --_count;
+        }
+    private:
+        Mutex _mutex;
+        uint32 _count;
+    };
+
+    T* _data;
+    RefCounter* _ref;
 };
 
 #endif /* !SHAREDPTR_H_ */
