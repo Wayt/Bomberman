340323c5e6caa3c0b1f9d3f4639c27827e58179b
masson_y <yoannm57@gmail.com>
2013-06-07 17:24:15 +0200

    menu merge

---
+++ Menu/menu.mk
@@ -0,0 +1,13 @@
+MENU_DIR 	=	./Menu/
+CXX     	+=	-I$(MENU_DIR)
+
+MENU_SRCS	= ./Menu/MenuMonitor.cpp	\
+		  ./Menu/MenuHandler.cpp	\
+		  ./Menu/MenuCamera.cpp		\
+		  ./Menu/SelectBox.cpp 		\
+		  ./Menu/Menu.cpp 		\
+		  ./Menu/SubMenu.cpp 		\
+		  ./Menu/SubObject.cpp 		\
+		  ./Menu/CheckBox.cpp 		\
+		  ./Menu/ListBox.cpp 		\
+		  ./Menu/InputBox.cpp
